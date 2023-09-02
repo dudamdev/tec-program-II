46e24260099035945ae752e3ed34eda1936963d5
Eduarda Matos <111284160+dudamdev@users.noreply.github.com>
2023-09-02 10:59:28 -0300

    Create Principal2

---
+++ Exercicio/src/exercicio/Principal2.java
@@ -0,0 +1,8 @@
+package exercicio;
+
+public class Principal2 {
+    public static void main(String[] args) {
+        Imposto imposto = new ImpostoPadrao();
+        imposto.calcularImposto(new Produto(), "AB");
+    }
+}
